cf309499a644c547a169488543d70fcdcda92407
tfrommen <info@tfrommen.de>
2017-01-04 16:14:48 +0100

    Tests: Adapt to new behavior.

--- tests/php/Unit/AvatarTest.php
+++ tests/php/Unit/AvatarTest.php
@@ -64,47 +64,50 @@ class AvatarTest extends TestCase {
 	}
 
 	/**
-	 * Tests if the passed avatar is returned for a wrong default.
+	 * Tests if the avatar is not replaced for a wrong default.
 	 *
-	 * @covers \tfrommen\AdorableAvatars\Avatar::filter_avatar()
-	 * @since  2.0.0
+	 * @covers \tfrommen\AdorableAvatars\Avatar::replace_avatar()
+	 * @since  2.1.0
 	 *
 	 * @return void
 	 */
-	public function test_return_passed_avatar_for_wrong_default() {
+	public function test_not_replacing_avatar_for_wrong_default() {
 
 		$avatar = 'some-avatar-here';
 
-		$this->assertSame( $avatar, ( new Testee() )->filter_avatar( $avatar, null, null, null, null, [] ) );
+		$this->assertSame( $avatar, ( new Testee() )->replace_avatar( $avatar, null, [ 'default' => 'wrong' ] ) );
 	}
 
 	/**
-	 * Tests if a found avatar is returned when not forcing the default.
+	 * Tests if the avatar is not replaced when it is not forced to.
 	 *
-	 * @covers \tfrommen\AdorableAvatars\Avatar::filter_avatar()
-	 * @since  2.0.0
+	 * @covers \tfrommen\AdorableAvatars\Avatar::replace_avatar()
+	 * @since  2.1.0
 	 *
 	 * @return void
 	 */
-	public function test_return_found_avatar_when_not_forcing_default() {
+	public function test_not_replacing_avatar_when_not_forced() {
+
+		Monkey\WP\Filters::expectApplied( 'adorable_avatars.force' )
+			->andReturn( false );
 
 		$avatar = 'some-avatar-here';
 
-		$this->assertSame( $avatar, ( new Testee() )->filter_avatar( $avatar, null, null, Testee::NAME, null, [
-			'force_default' => false,
-			'found_avatar'  => true,
-		] ) );
+		$this->assertSame( $avatar, ( new Testee() )->replace_avatar( $avatar, null, [ 'default' => Testee::NAME ] ) );
 	}
 
 	/**
-	 * Tests if an avatar with the expected data is returned.
+	 * Tests if the avatar is replaced as expected.
 	 *
-	 * @covers \tfrommen\AdorableAvatars\Avatar::filter_avatar()
+	 * @covers \tfrommen\AdorableAvatars\Avatar::replace_avatar()
 	 * @since  2.0.0
 	 *
 	 * @return void
 	 */
-	public function test_return_avatar_with_expected_data() {
+	public function test_replace_avatar() {
+
+		Monkey\Functions::when( 'is_ssl' )
+			->justReturn();
 
 		Monkey\Functions::when( 'esc_url' )
 			->returnArg();
@@ -121,9 +124,16 @@ class AvatarTest extends TestCase {
 		$args = [
 			'class'      => 'some classes here',
 			'extra_attr' => 'some="attr"',
+			'size'       => $size,
+			'alt'        => $alt,
+			'default'    => Testee::NAME,
 		];
 
-		$avatar = ( new Testee() )->filter_avatar( null, $id_or_email, $size, Testee::NAME, $alt, $args );
+		Monkey\WP\Filters::expectApplied( 'adorable_avatars.force' )
+			->with( false, $id_or_email, $args )
+			->andReturn( true );
+
+		$avatar = ( new Testee() )->replace_avatar( null, $id_or_email, $args );
 
 		$this->assertNotFalse( strpos( $avatar, ' width="' . $size . '"' ) );
 
@@ -158,7 +168,7 @@ class AvatarTest extends TestCase {
 	/**
 	 * Tests if an avatar with the expected URLs is returned.
 	 *
-	 * @covers       \tfrommen\AdorableAvatars\Avatar::filter_avatar()
+	 * @covers       \tfrommen\AdorableAvatars\Avatar::replace_avatar()
 	 * @dataProvider identifier_data_provider
 	 * @since        2.0.0
 	 *
@@ -169,6 +179,14 @@ class AvatarTest extends TestCase {
 	 * @return void
 	 */
 	public function test_return_avatar_with_expected_urls( $id_or_email, $identifier, $user ) {
+
+		Monkey\WP\Filters::expectApplied( 'adorable_avatars.force' )
+			->andReturn( true );
+
+		$is_ssl = (bool) rand( 0, 1 );
+
+		Monkey\Functions::when( 'is_ssl' )
+			->justReturn( $is_ssl );
 
 		Monkey\Functions::when( 'esc_url' )
 			->returnArg();
@@ -182,11 +200,21 @@ class AvatarTest extends TestCase {
 
 		$size = rand( 0, 400 );
 
-		$avatar = ( new Testee() )->filter_avatar( null, $id_or_email, $size, Testee::NAME, null, [] );
+		$avatar = ( new Testee() )->replace_avatar( null, $id_or_email, [
+			'size'    => $size,
+			'default' => Testee::NAME,
+			'alt'     => null,
+		] );
 
 		$this->assertRegExp( '~ src="[^"]+/' . $size . '/' . $identifier . '\.png"~', $avatar );
 
 		$this->assertRegExp( '~ srcset="[^"]+/' . ( 2 * $size ) . '/' . $identifier . '\.png 2x"~', $avatar );
+
+		$this->assertSame( $is_ssl, false !== strpos( $avatar, ' src="https://' ) );
+		$this->assertSame( $is_ssl, false === strpos( $avatar, ' src="http://' ) );
+
+		$this->assertSame( $is_ssl, false !== strpos( $avatar, ' srcset="https://' ) );
+		$this->assertSame( $is_ssl, false === strpos( $avatar, ' srcset="http://' ) );
 	}
 
 	/**
@@ -196,6 +224,8 @@ class AvatarTest extends TestCase {
 	 */
 	public function identifier_data_provider() {
 
+		$email = 'someone@example.com';
+
 		$post = Mockery::mock( 'WP_Post' );
 
 		$post->post_author = 42;
@@ -204,11 +234,17 @@ class AvatarTest extends TestCase {
 
 		$comment->user_id = 42;
 
+		$guest_comment = Mockery::mock( 'WP_Comment' );
+
+		$guest_comment->comment_author_email = $email;
+
+		$guest_comment->user_id = 0;
+
 		$user = Mockery::mock( 'WP_User' );
 
 		$user->ID = 42;
 
-		$user->user_email = 'someone@example.com';
+		$user->user_email = $email;
 
 		$identifier = md5( strtolower( $user->user_email ) );
 
@@ -248,6 +284,11 @@ class AvatarTest extends TestCase {
 				'identifier'  => $identifier,
 				'user'        => $user,
 			],
+			'guest_comment'  => [
+				'id_or_email' => $guest_comment,
+				'identifier'  => $identifier,
+				'user'        => $user,
+			],
 			'user'           => [
 				'id_or_email' => $user,
 				'identifier'  => $identifier,
@@ -259,7 +300,7 @@ class AvatarTest extends TestCase {
 	/**
 	 * Tests if the returned avatar has the default class.
 	 *
-	 * @covers       \tfrommen\AdorableAvatars\Avatar::filter_avatar()
+	 * @covers       \tfrommen\AdorableAvatars\Avatar::replace_avatar()
 	 * @dataProvider avatar_with_default_class_args_provider
 	 * @since        2.0.0
 	 *
@@ -269,13 +310,23 @@ class AvatarTest extends TestCase {
 	 */
 	public function test_return_avatar_with_default_class( array $args ) {
 
+		Monkey\WP\Filters::expectApplied( 'adorable_avatars.force' )
+			->andReturn( true );
+
+		Monkey\Functions::when( 'is_ssl' )
+			->justReturn();
+
 		Monkey\Functions::when( 'esc_url' )
 			->returnArg();
 
 		Monkey\Functions::when( 'esc_attr' )
 			->returnArg();
 
-		$avatar = ( new Testee() )->filter_avatar( null, null, null, Testee::NAME, null, $args );
+		$avatar = ( new Testee() )->replace_avatar( null, null, array_merge( $args, [
+			'default' => Testee::NAME,
+			'size'    => null,
+			'alt'     => null,
+		] ) );
 
 		$this->assertSame( 1, preg_match( '~ class="(?:[^"]*\s)?avatar-default(?:\s|")~', $avatar ) );
 	}
@@ -302,5 +353,58 @@ class AvatarTest extends TestCase {
 				],
 			],
 		];
+	}
+
+	/**
+	 * Tests if the default argument is not replaced for a wrong default.
+	 *
+	 * @covers \tfrommen\AdorableAvatars\Avatar::replace_default()
+	 * @since  2.1.0
+	 *
+	 * @return void
+	 */
+	public function test_not_replacing_default_argument_for_wrong_default() {
+
+		$this->assertSame( null, ( new Testee() )->replace_default( null, null, null, 'wrong' ) );
+	}
+
+	/**
+	 * Tests if the returned avatar has the expected default URL.
+	 *
+	 * @covers       \tfrommen\AdorableAvatars\Avatar::replace_default()
+	 * @dataProvider identifier_data_provider
+	 * @since        2.1.0
+	 *
+	 * @param mixed  $id_or_email User identifier.
+	 * @param string $identifier  User identifier string.
+	 * @param object $user        User object.
+	 *
+	 * @return void
+	 */
+	public function test_return_avatar_with_expected_default_url( $id_or_email, $identifier, $user ) {
+
+		$is_ssl = (bool) rand( 0, 1 );
+
+		Monkey\Functions::when( 'is_ssl' )
+			->justReturn( $is_ssl );
+
+		Monkey\Functions::expect( 'get_user_by' )
+			->withArgs( [ 'id', $user->ID ] )
+			->andReturn( $user );
+
+		$size = rand( 0, 400 );
+
+		$avatar = ( new Testee() )->replace_default(
+			'https://example.com/?foo=bar&d=' . Testee::NAME . '&baz=qux',
+			$id_or_email,
+			$size,
+			Testee::NAME
+		);
+
+		$protocol = preg_quote( urlencode( ( $is_ssl ? 'https' : 'http' ) . '://' ) );
+
+		$url = preg_quote( urlencode( "/{$size}/{$identifier}.png" ) );
+
+		$this->assertRegExp( "~&d={$protocol}[^&]+{$url}&~", $avatar );
 	}
 }
