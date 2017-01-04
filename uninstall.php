9b48622919135cf508b7c394f99db2ccbbcb352c
Thorsten Frommen <info@tfrommen.de>
2017-01-04 10:41:02 +0100

    Uninstall: Require file with Avatar class. #4
    Close #4.

--- uninstall.php
+++ uninstall.php
@@ -6,6 +6,11 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	return;
 }
 
+/**
+ * Avatar model.
+ */
+require_once __DIR__ . '/src/Avatar.php';
+
 if ( Avatar::NAME === get_option( 'avatar_default' ) ) {
 	update_option( 'avatar_default', 'mystery' );
 }
